15fe6e9422d4a23e5cdfa8bf1e32e4e934f81eff
Aleksandr Pasechnik <apasechnik@mediamath.com>
2017-11-22 15:18:05 -0500

    initial commit

---
+++ main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+	"regexp"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatalf("usage: %s filename.json", os.Args[0])
+	}
+
+	filename := os.Args[1]
+
+	data, err := readFile(filename)
+	if err != nil {
+		log.Fatalf("failed to read file: %v", err)
+	}
+
+	if quickCheckJSON(data) == nil {
+		log.Printf("already valid json")
+		return
+	}
+
+	processed, err := cleanTrailingCommas(data)
+	if err != nil {
+		log.Fatalf("failed to clean file: %v", err)
+	}
+
+	if err := quickCheckJSON(processed); err != nil {
+		log.Fatalf("json still invalid: %v\n%s", err, processed)
+	}
+
+	bkpFilename, err := writeBackup(filename, data)
+	if err != nil {
+		log.Printf("failed to write backup: %v", err)
+	}
+
+	err = writeFile(filename, processed)
+	if err != nil {
+		log.Fatalf("failed to write processed data: %v", err)
+	}
+
+	os.Remove(bkpFilename)
+}
+
+func readFile(filename string) ([]byte, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	return ioutil.ReadAll(f)
+}
+
+func writeFile(filename string, data []byte) error {
+	f, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+
+	_, err = f.Write(data)
+	if err != nil {
+		f.Close()
+		return err
+	}
+
+	return f.Close()
+}
+
+func writeBackup(filename string, data []byte) (string, error) {
+	bkpFilename := filename + ".bkp"
+
+	f, err := os.Create(bkpFilename)
+	if err != nil {
+		return "", err
+	}
+
+	_, err = f.Write(data)
+	if err != nil {
+		f.Close()
+		return "", err
+	}
+
+	return bkpFilename, f.Close()
+}
+
+func quickCheckJSON(data []byte) error {
+	var stuff interface{}
+	return json.Unmarshal(data, &stuff)
+}
+
+func cleanTrailingCommas(data []byte) ([]byte, error) {
+	re, err := regexp.Compile(`(?ms),(\s*[}\]])`)
+	if err != nil {
+		return nil, err
+	}
+
+	return re.ReplaceAll(data, []byte("$1")), nil
+}
---
+++ main_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCleanTrailingCommas(t *testing.T) {
+	cases := []struct {
+		name   string
+		input  string
+		output string
+	}{
+		{
+			name:   "nothing to do",
+			input:  `{"foo": "bar"}`,
+			output: `{"foo": "bar"}`,
+		},
+		{
+			name:   "trailing comma in object",
+			input:  `{"foo": "bar",}`,
+			output: `{"foo": "bar"}`,
+		},
+		{
+			name:   "trailing comman in array",
+			input:  `[1,2,3,]`,
+			output: `[1,2,3]`,
+		},
+		{
+			name: "newlines don't matter",
+			input: `{
+				"foo": 1,
+			}`,
+			output: `{
+				"foo": 1
+			}`,
+		},
+	}
+
+	for _, c := range cases {
+		c := c
+		t.Run(c.name, func(t *testing.T) {
+			out, err := cleanTrailingCommas([]byte(c.input))
+			if err != nil {
+				t.Errorf("unexpected error: %v", err)
+			}
+
+			if string(out) != c.output {
+				t.Errorf("incorrect output: %s", out)
+			}
+		})
+	}
+}
